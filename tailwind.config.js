89b2b5d8b2e7a09aac726cb336391bb083486214
Matthew Blott <m4ttb@pm.me>
2023-06-19 16:25:46 +0100

    saved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-const forms = require("@tailwindcss/forms");
+// const forms = require("@tailwindcss/forms");
 const typography = require("@tailwindcss/typography");
 
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
-  plugins: [typography, forms],
+  plugins: [typography],
 };
